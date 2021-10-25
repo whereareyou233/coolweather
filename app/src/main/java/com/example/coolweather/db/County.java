23326b0ef14245dda2150d040b83de150b442cfa
zhangheng <3232876513@qq.com>
2021-10-25 19:30:24 +0800

    first submit

---
+++ app/src/main/java/com/example/coolweather/db/County.java
@@ -0,0 +1,45 @@
+package com.example.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+
+
+public class County extends DataSupport {
+    private String countyName;
+
+    private String weatherId;
+    private int id;
+    private int cityId;
+
+    public String getCountyName() {
+        return countyName;
+    }
+
+    public void setCountyName(String countyName) {
+        this.countyName = countyName;
+    }
+
+    public String getWeatherId() {
+        return weatherId;
+    }
+
+    public void setWeatherId(String weatherId) {
+        this.weatherId = weatherId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+}
